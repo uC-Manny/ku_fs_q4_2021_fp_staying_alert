508abc680d0abcdff53ce6a2ba57fc9e518c29a4
uc-Manny <manny.muro@gmail.com>
2022-06-18 23:48:20 -0700

    Updated the schema models to take a number for the various ID fields and holding off on making a relational link between the various arrays.

--- server/models/transactions.model.js
+++ server/models/transactions.model.js
@@ -5,12 +5,15 @@ const unique_active = false;
 const Transaction = new mongoose.Schema({
   id : {type:Number, required: require_active, unique: unique_active},
   id_num : {type:Number, required: require_active, unique: unique_active},
-  user_id: {type:mongoose.Schema.Types.ObjectId, ref:"UserData", required: require_active},
+  //user_id: {type:mongoose.Schema.Types.ObjectId, ref:"UserData", required: require_active},
+  user_id: {type:Number, required: require_active},
   trans_type: {type:String},
   message_sent: {type:String},
-  person_supported_id: {type:mongoose.Schema.Types.ObjectId, ref:"PersonData"},
+  //person_supported_id: {type:mongoose.Schema.Types.ObjectId, ref:"PersonData"},
+  person_supported_id: {type:Number, required: require_active},
   trans_date: {type:Date, required: require_active},
-  person_id_arr: [{type:mongoose.Schema.Types.ObjectId, ref:"PersonData"}],
+  //person_id_arr: [{type:mongoose.Schema.Types.ObjectId, ref:"PersonData"}],
+  person_id_arr: [{type:Number, required: require_active}],
   system_gen_act_notes: {type:String},
 }, 
 {collection: 'transactions'}
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,8 +11,10 @@ const User = new mongoose.Schema({
   email_addr: {type:String, required: require_active, unique: unique_active},
   password_hash: {type:String, required: require_active},
   emergency_msg: {type:String},
-  emer_person_id: {type:mongoose.Schema.Types.ObjectId, ref:"PersonData"},
-  emer_group_id: {type:mongoose.Schema.Types.ObjectId, ref:"GroupsData"},
+  //emer_person_id: {type:mongoose.Schema.Types.ObjectId, ref:"PersonData"},
+  emer_person_id: {type:Number, required: require_active},
+  //emer_group_id: {type:mongoose.Schema.Types.ObjectId, ref:"GroupsData"},
+  emer_group_id: {type:Number, required: require_active},
   loc_interval_in_mins: {type:Number},
 }, 
 {collection: 'user_account', timestamps: true}
