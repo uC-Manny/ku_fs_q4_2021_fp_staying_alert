9e579f257f9d90eb03b2195edef0d4c82e638bcd
StephenSvetlovics <battousai012083@gmail.com>
2022-06-18 22:40:30 -0400

    forgot to save before last commit

--- server/index.js
+++ server/index.js
@@ -22,39 +22,41 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-// app.post("/api/register", async (req, res) => {
-//   const users = await User.find({}); // Get all the users
-//   const num_users = users.length;
-//   const new_user_num = users[num_users-1].id_num ? users[num_users-1].id_num + 1 : 1; // Look at the last record for the next number
-//   console.log("Number of users = " + users.length);
-//   console.log("New User # = " + new_user_num);
-//   let bpassword = "";
-//   let rpassword = "";
-//   bpassword = req.body.password;
-//   rpassword = req.body.rpassword;
-//   if (bpassword !== rpassword) {
-//       err = "!!!Passwords do not match!!!"
-//       res.json({ status: "error", error: err });
-//   } else {
-//     try {
-//       const salt = bcrypt.genSaltSync(saltRounds);
-//       const hash = bcrypt.hashSync(bpassword, salt);
-//       const user = await User.create({
-//         user_name: req.body.uname,
-//         first_name: req.body.fname,
-//         last_name: req.body.lname,
-//         email_addr: req.body.email,
-//         password_hash: hash,
-//         id_num: new_user_num,
-//       });
-//         user.save();
-//         res.json({ status: "ok" });
-//     } catch (err) {
-//       console.log("error is: ", err);
-//       res.json({ status: "error", error: err });
-//     }
-//   }
-// });
+app.post("/api/register", async (req, res) => {
+  const users = await User.find({}); // Get all the users
+  const num_users = users.length;
+  const new_user_num = users[num_users - 1].id_num
+    ? users[num_users - 1].id_num + 1
+    : 1; // Look at the last record for the next number
+  console.log("Number of users = " + users.length);
+  console.log("New User # = " + new_user_num);
+  let bpassword = "";
+  let rpassword = "";
+  bpassword = req.body.password;
+  rpassword = req.body.rpassword;
+  if (bpassword !== rpassword) {
+    err = "!!!Passwords do not match!!!";
+    res.json({ status: "error", error: err });
+  } else {
+    try {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hash = bcrypt.hashSync(bpassword, salt);
+      const user = await User.create({
+        user_name: req.body.uname,
+        first_name: req.body.fname,
+        last_name: req.body.lname,
+        email_addr: req.body.email,
+        password_hash: hash,
+        id_num: new_user_num,
+      });
+      user.save();
+      res.json({ status: "ok" });
+    } catch (err) {
+      console.log("error is: ", err);
+      res.json({ status: "error", error: err });
+    }
+  }
+});
 
 app.post("/api/login", async (req, res) => {
   const user = await User.findOne({
