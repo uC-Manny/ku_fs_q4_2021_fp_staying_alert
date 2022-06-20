70c8af31d8fd9bc4020b2222e775a5a44f1024e4
StephenSvetlovics <battousai012083@gmail.com>
2022-06-20 19:45:46 -0400

    final changes

--- client/src/components/pages/DashboardPage.jsx
+++ client/src/components/pages/DashboardPage.jsx
@@ -1,10 +1,35 @@
 import Button from "../buttons/Button";
+import { Link } from "react-router-dom";
 
 export default function DashboardPage() {
   return (
     <>
       <div className="dashboard">
-        <h1>Welcome User!</h1>
+        <h1>Welcome kaijudono!</h1>
+
+        <Button buttonStyle="btn-success">
+          <Link
+            style={{
+              color: "white",
+            }}
+            to="/create_person"
+          >
+            Create Person
+          </Link>
+        </Button>
+        {"  "}
+        <Button>
+          <Link
+            style={{
+              color: "white",
+            }}
+            to="/"
+          >
+            Logout
+          </Link>
+        </Button>
+        <br />
+        <br />
         <Button
           onClick={() => {
             alert("Alert sent to every person in your list!");
